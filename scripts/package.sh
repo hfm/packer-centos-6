6c92d1f26d1d6ddc51e97e61ec7786859f258742
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-11-13 20:25:03 +0900

    Remove package provision because "default CentOS" has no epel repository

--- scripts/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-osarch=`uname -i`
-
-# epel
-rpm --import "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/RPM-GPG-KEY-EPEL-6"
-rpm -ivh "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/$osarch/epel-release-6-8.noarch.rpm"
